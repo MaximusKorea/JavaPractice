9f0ca59604d861fec84ba08e581ce01b33bf6dd1
JohnYang <john4081@naver.com>
2021-02-18 02:46:25 +0900

    stream 연습

--- src/stream1/ListStream.java
+++ src/stream1/ListStream.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 public class ListStream {
 
 	public static void main(String[] args) {
-		//컬렉션 인스턴스를 대상으로 stream 메소드를 호출할 수 있다.
+		//컬렉션 인스턴스(List,Map,Set)를 대상으로 stream 메소드를 호출할 수 있다.
 		List<String> list = Arrays.asList("Airplane","Subway","Train");
 		list.stream().forEach(s -> System.out.print(s + "\t"));
 		System.out.println();
